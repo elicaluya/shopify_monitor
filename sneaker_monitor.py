540d1cd767e73a91b3722e3350bbe0a26fda7c9e
Elijah Caluya <elicaluya@yahoo.com>
2020-12-09 18:27:15 -0800

    added autofill and checkout capability

--- sneaker_monitor.py
+++ sneaker_monitor.py
@@ -22,12 +22,26 @@ def availabilityCheck():
 driver = webdriver.Chrome(executable_path = r'C:\Users\elica\eclipse-workspace\SeleniumFramework\ChromeDriver\chromedriver.exe')
 
 driver.get('https://shop-usa.palaceskateboards.com/products/f0epjdj4n3lr')
+
+# selecting item on product page
 driver.find_element_by_xpath("//select/option[@value='35703051014']").click()
 driver.find_element_by_xpath("//input[@class='add cart-btn clearfix']").click()
-
 driver.implicitly_wait(10)
-# WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//a[@class='cart-heading']"))).click()
-
 driver.find_element_by_xpath("//a[@class='cart-heading']").click()
-driver.find_element_by_xpath("//input[@class='checkbox-input']").click()
+
+# On cart page
+checkbox = driver.find_element_by_xpath("//input[@class='checkbox-input']")
+driver.execute_script("arguments[0].click();", checkbox)
 driver.find_element_by_xpath("//input[@class='shopping-btn']").click()
+
+# On checkout page
+driver.find_element_by_xpath("//input[@placeholder='Email']").send_keys("example@gmail.com")
+driver.find_element_by_xpath("//input[@placeholder='First name']").send_keys("Joe")
+driver.find_element_by_xpath("//input[@placeholder='Last name']").send_keys("Mama")
+driver.find_element_by_xpath("//input[@placeholder='Address']").send_keys("123 First st.")
+driver.find_element_by_xpath("//input[@placeholder='Apartment, suite, etc. (optional)']").send_keys("Apt. A")
+driver.find_element_by_xpath("//input[@placeholder='City']").send_keys("Dublin")
+driver.find_element_by_xpath("//select[@id='checkout_shipping_address_country']/option[text()='United States']").click()
+driver.find_element_by_xpath("//select[@id='checkout_shipping_address_province']/option[text()='California']").click()
+driver.find_element_by_xpath("//input[@placeholder='ZIP code']").send_keys("94568")
+driver.find_element_by_xpath("//input[@placeholder='Phone']").send_keys("1234567891")
